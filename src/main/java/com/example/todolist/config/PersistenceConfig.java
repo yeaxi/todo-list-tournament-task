7d711de9f4571e75083f047320f35ae7e269c16c
yeaxi <yeaxi98@gmail.com>
2018-05-04 11:28:14 +0300

    -remove persistence config

--- src/main/java/com/example/todolist/config/PersistenceConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.todolist.config;
-
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@EnableJpaRepositories(basePackages = "com.example.todolist.repository")
-public class PersistenceConfig {
-
-}
